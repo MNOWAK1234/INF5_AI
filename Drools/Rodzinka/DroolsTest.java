e0fcbda1b3b05a7531a757268180ce6cf96f657a
MNOWAK1234 <108874597+MNOWAK1234@users.noreply.github.com>
2023-12-03 11:57:16 +0100

    Update DroolsTest.java

--- Drools/Rodzinka/DroolsTest.java
+++ Drools/Rodzinka/DroolsTest.java
@@ -1,28 +1,54 @@
-public static enum Plec {NIEZNANA,MEZCZYZNA,KOBIETA};
+package com.sample;
 
-public static class Osoba {
-        public String imie;
-        public Plec plec;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.logger.*;
 
-        public Osoba(String imie) {
-                this.imie=imie;
-                this.plec=Plec.NIEZNANA;
-        }       
+public class DroolsTest {
 
-        public String toString() {
-                return imie;
+	public static final void main(String[] args) {
+        try {
+            // load up the knowledge base
+	        KieServices ks = KieServices.Factory.get();
+    	    KieContainer kContainer = ks.getKieClasspathContainer();
+        	KieSession kSession = kContainer.newKieSession("ksession-rules");
+        	KieRuntimeLogger kLogger = ks.getLoggers().newFileLogger(kSession,  "test");
+            // go !
+            kSession.fireAllRules();
+            kLogger.close();
+        } catch (Throwable t) {
+            t.printStackTrace();
         }
-}
+    }
+    
+	public static enum Plec {NIEZNANA,MEZCZYZNA,KOBIETA};
 
-public static class Relacja {
+	public static class Osoba {
+	        public String imie;
+	        public Plec plec;
 
-    public Osoba[] osoby=new Osoba[2];
-    public String relacja;
+	        public Osoba(String imie) {
+	                this.imie=imie;
+	                this.plec=Plec.NIEZNANA;
+	        }       
 
-    public Relacja(Osoba o1, Osoba o2, String relacja) {
-        this.osoby[0]=o1;
-        this.osoby[1]=o2;
-        this.relacja=relacja;
-    }   
+	        public String toString() {
+	                return imie;
+	        }
+	}
+
+	public static class Relacja {
+
+	    public Osoba[] osoby=new Osoba[2];
+	    public String relacja;
+
+	    public Relacja(Osoba o1, Osoba o2, String relacja) {
+	        this.osoby[0]=o1;
+	        this.osoby[1]=o2;
+	        this.relacja=relacja;
+	    }   
+
+	}
 
 }
