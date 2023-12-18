01e43bd1f97c1a67e441e9a4d42f0ab0eb2b4095
MNOWAK1234 <108874597+MNOWAK1234@users.noreply.github.com>
2023-12-18 20:12:00 +0100

    Add files via upload

---
+++ Drools/ExpertSystem/src/main/java/com/sample/Window.java
@@ -0,0 +1,24 @@
+package com.sample;
+import javax.swing.JOptionPane;
+
+public class Window {
+	
+	public Window(){}
+	
+	public static String askUser(String question, String[] options) {
+		int n = JOptionPane.showOptionDialog(
+				null,							//parent
+				question,						//Dialog message
+				"Jaka_planeta_wariacie?",		//Dialog title
+				JOptionPane.YES_NO_OPTION,		//option type
+				JOptionPane.QUESTION_MESSAGE,	//message type
+				null,     						//icon
+				options,  						//options
+				options[0]					//initial value
+			); 
+			if (n<0 || n >= options.length)
+				return "User interruption";
+			else 
+				return options[n];
+		}			
+}
